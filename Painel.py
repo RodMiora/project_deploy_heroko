1e1978fd264f290b501cf65269c38d4294b31f97
RodMiora <rodrigomiorin@gmail.com>
2024-05-17 20:45:58 -0300

    mudança na pagina de login

--- Painel.py
+++ Painel.py
@@ -10,16 +10,24 @@ from streamlit_option_menu import option_menu
 # Importando Banco de Dados do SupaBase através de uma variável de ambiente permanente
 supabase: Client = create_client(SUBABASE_URL, SUBABASE_API_KEY)
 
-
 # Função para configurar o título da página
 def set_page_title(title):
     st.session_state['Bem Vindo'] = title
     st.markdown(f'<h1 style="font-weight:bold;">{title}</h1>', unsafe_allow_html=True)
 
-
 # Configurando a página
 st.set_page_config(page_title="Sua Sala", page_icon=":bar_chart:", layout="wide")
 
+# Criando espaço em branco para centralizar a caixa de login
+st.write("")
+st.write("")
+st.write("")
+
+# Definindo o título da página
+st.title("Página de Login")
+
+# Centralizando a caixa de login
+col1, col2, col3 = st.columns([1, 3, 1])
 
 # Carregando o arquivo de configuração YAML
 try:
@@ -29,13 +37,11 @@ except FileNotFoundError:
     print("O arquivo YAML não foi encontrado.")
     config = None
 
-
 # Verificando se o arquivo de configuração foi carregado corretamente
 if config is not None:
     credentials = config['credentials']
 else:
     print("Falha ao carregar o arquivo YAML.")
-
 
 # Inicializando o objeto de autenticação com a classe correta
 authenticator = stauth.Authenticate(
@@ -68,37 +74,35 @@ if authentication_status:
     if selected == "Painel":
         st.title(f"Você Selecionou {selected}")
 
+        # Página do Dashboard
+        if selected == 'Painel':
+            # Carregar dados para o dashboard
+            uploaded_file = st.file_uploader("Carregar banco de dados", type=["csv", "xlsx"])
+            if uploaded_file is not None:
+                df = pd.read_csv(uploaded_file)
+                # Exibindo todos os gráficos
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.subheader('Gráfico de Linhas')
+                    fig_line = px.line(df)
+                    st.plotly_chart(fig_line)
+                with col2:
+                    st.subheader('Gráfico de Barras')
+                    fig_bar = px.bar(df)
+                    st.plotly_chart(fig_bar)
+                col3, col4 = st.columns(2)
+                with col3:
+                    st.subheader('Histograma')
+                    fig_hist = px.histogram(df)
+                    st.plotly_chart(fig_hist)
+                with col4:
+                    st.subheader('Gráfico de Pontos')
+                    fig_scatter = px.scatter(df)
+                    st.plotly_chart(fig_scatter)
     
-
-    # Página do Dashboard
-    if selected == 'Painel':
-        # Carregar dados para o dashboard
-        uploaded_file = st.file_uploader("Carregar banco de dados", type=["csv", "xlsx"])
-        if uploaded_file is not None:
-            df = pd.read_csv(uploaded_file)
-            # Exibindo todos os gráficos
-            col1, col2 = st.columns(2)
-            with col1:
-                st.subheader('Gráfico de Linhas')
-                fig_line = px.line(df)
-                st.plotly_chart(fig_line)
-            with col2:
-                st.subheader('Gráfico de Barras')
-                fig_bar = px.bar(df)
-                st.plotly_chart(fig_bar)
-            col3, col4 = st.columns(2)
-            with col3:
-                st.subheader('Histograma')
-                fig_hist = px.histogram(df)
-                st.plotly_chart(fig_hist)
-            with col4:
-                st.subheader('Gráfico de Pontos')
-                fig_scatter = px.scatter(df)
-                st.plotly_chart(fig_scatter)
-    
-   
 elif authentication_status == False:
     st.error('Nome de usuário/senha incorreto')
 elif authentication_status == None:
     st.warning('Por favor, insira seu nome de usuário e senha')
+
 
